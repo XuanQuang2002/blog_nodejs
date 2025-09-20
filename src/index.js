fadd48e1ce4df9a7d73aaddbb8c85588d9920291
XuanQuang2002 <quangphamxuan61@gmail.com>
2025-09-21 00:28:15 +0700

    add view

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import morgan from 'morgan'
+import { engine } from 'express-handlebars'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express()
+const port = 3000
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+}))
+app.set('view engine', 'hbs')
+
+// Set views files location
+var pathToViews = path.join(__dirname, 'resources', 'views')
+app.set('views', pathToViews)
+
+// HTTP loggers
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
